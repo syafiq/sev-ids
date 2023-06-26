3dbfec9c9166f51bb9615e4116c41e5a4c5eebee
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2023-06-26 11:04:38 +0000

    the script to generate simulation should suffice with the first two scenarios now.

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 CPUS=32
-PKTS="1000"
 CLONE_SKB="clone_skb 0"
-PKT_SIZE="pkt_size 1024"
+PKT_SIZE="pkt_size $1"
 COUNT="count 0"
 DELAY="delay 0"
 ETH="tap0"
-RATEP="1000000"
+RATEP="$2"
+FLOWS="$3"
 
 function pgset() {
     local result
-    echo $1
     echo $1 > $PGDEV
     result=`cat $PGDEV | fgrep "Result: OK:"`
     if [ "$result" = "" ]; then
@@ -22,17 +21,17 @@ function pgset() {
 for ((processor=0;processor<$CPUS;processor++))
 do
     PGDEV=/proc/net/pktgen/kpktgend_$processor
-    echo "Removing all devices"
+    #echo "Removing all devices"
     pgset "rem_device_all"
 done
 
 for ((processor=0;processor<$CPUS;processor++))
 do
     PGDEV=/proc/net/pktgen/kpktgend_$processor
-    echo "Adding $ETH"
+    #echo "Adding $ETH"
     pgset "add_device $ETH@$processor"
     PGDEV=/proc/net/pktgen/$ETH@$processor
-    echo "Configuring $PGDEV"
+    #echo "Configuring $PGDEV"
     pgset "$COUNT"
     pgset "flag QUEUE_MAP_CPU"
     pgset "$CLONE_SKB"
@@ -40,13 +39,12 @@ do
     pgset "$PKT_SIZE"
     pgset "$DELAY"
     pgset "ratep $RATEP"
-    #pgset "burst 10000"
     pgset "dst 1.1.1.1"
     pgset "dst_mac 52:55:00:d1:55:01"
     pgset "udp_dst_min 10000"
     pgset "udp_dst_max 50000"
     pgset "flag IPDST_RND"
-    pgset "flows 32000"
+    pgset "flows $FLOWS"
     pgset "flowlen 16"
 done
 
---
+++ generator/rungen.sh
@@ -0,0 +1,17 @@
+for psize in 64 128 256 512 1024
+do	
+	for tcpflows in 256 1000 4000 8000 16000 32000
+	do 
+		for ratep in 100 1000 10000 100000 1000000
+		do
+			echo "Starting simulation with psize $psize tcpflows $tcpflows ratep $ratep"
+			echo "====================================================================="
+			/usr/bin/taskset --cpu-list 32 /usr/bin/ssh -p 2222 root@127.0.0.1 '/usr/bin/taskset --cpu-list 0 /home/syafiq/snort/bin/snort --daq afpacket -i enp0s3 --daq-var buffer_size_mb=512 -z 1 -A fast' &
+ 			/usr/bin/taskset --cpu-list 0-31 /usr/bin/timeout 120 /home/syafiq/sev-ids/generator/generate.sh $psize $ratep $tcpflows
+ 			/usr/bin/taskset --cpu-list 32 /usr/bin/ssh -p 2222 root@127.0.0.1 '/usr/bin/killall snort' &
+			echo "====================================================================="
+			echo "Ending simulation with psize $psize tcpflows $tcpflows ratep $ratep"
+			/usr/bin/sleep 5
+		done
+	done
+done
